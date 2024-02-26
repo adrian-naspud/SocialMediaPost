973f03e7d515bf871c8149be027098ff2dad6ecf
adrian-naspud <106284495+adrian-naspud@users.noreply.github.com>
2024-02-26 13:52:15 -0500

    init commit

---
+++ users.js
@@ -0,0 +1,48 @@
+const userData = [
+  {
+    username: "m.verstappen",
+    profilePicture: "assets/user1 vertappen.png",
+    comment: "Thanks to all my fans for supporting me",
+    likedPost: true
+  },
+  {
+    username: "s.perez",
+    profilePicture: "assets/user2 perez.png",
+    comment: "Viva Mexico!",
+    likedPost: true
+  },
+  {
+    username: "l.hamilton",
+    profilePicture: "assets/user3 hamilton.png",
+    comment: "I need a bo'ohw'o'wo'er ",
+    likedPost: true
+  },
+  {
+    username: "f.alonso",
+    profilePicture: "assets/user4 alonso.png",
+    comment: "Check out my new car "
+  },
+  {
+    username: "c.sainz",
+    profilePicture: "assets/user5 sainz.png",
+    comment: "Ferari doesnt help ",
+    likedPost: true
+  },
+  {
+    username: "c.leclerc",
+    profilePicture: "assets/user6 leclerc.png",
+    comment: "Hi mom"
+  },
+  {
+    username: "g.russell",
+    profilePicture: "assets/user7 russel.png",
+    comment: "WOOOOOO"
+  },
+  {
+    username: "l.norris",
+    profilePicture: "assets/user8 norris.png",
+    comment: "I just want more experience and a faster car "
+  }
+]
+
+export { userData };
